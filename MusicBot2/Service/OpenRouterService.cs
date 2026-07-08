89109176ea8bd5a4d7a9ed126f3f91de49da0e41
Tommy_Tsai <Tommy_Tsai@asus.com>
2026-07-08 16:48:22 +0800

    改新model

--- MusicBot2/Service/OpenRouterService.cs
+++ MusicBot2/Service/OpenRouterService.cs
@@ -55,9 +55,16 @@ namespace MusicBot2.Service
         // 所以後面排了幾個走不同 provider 的模型作保底。
         private readonly string[] _models =
         {
+            // === 第0梯隊：短時間內免費 ===
+            "tencent/hy3:free",
+
+
+
             // === 第一梯隊：大型高品質模型 ===
-            "openrouter/owl-alpha",                       // 我這邊抓到官方頁面它其實還在榜上第一名！建議你重新測一次，可能只是暫時性問題
+            "nvidia/nemotron-3.5-content-safety:free",
+            //"openrouter/owl-alpha",                       // 我這邊抓到官方頁面它其實還在榜上第一名！建議你重新測一次，可能只是暫時性問題
             //"moonshotai/kimi-k2.6:free",                  // 官方頁面顯示還在免費榜，但你說沒成功傳出去過，可以再測一次
+            "google/gemma-4-26b-a4b-it:free",
             "nvidia/nemotron-3-super-120b-a12b:free",      // 120B MoE, 官方免費榜使用量#1(排除owl-alpha後)
             "nvidia/nemotron-3-ultra-550b-a55b:free",       // 550B MoE(55B啟用), 1M context 超長文本強
 
