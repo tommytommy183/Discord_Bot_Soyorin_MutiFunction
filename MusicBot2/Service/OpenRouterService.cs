e03af993f8e6ac1e3a563511c5d5dd054201c2b0
Tommy_Tsai <Tommy_Tsai@asus.com>
2026-07-30 15:30:58 +0800

    放上新的MoE模型進行test

--- MusicBot2/Service/OpenRouterService.cs
+++ MusicBot2/Service/OpenRouterService.cs
@@ -59,7 +59,7 @@ namespace MusicBot2.Service
         {
             // === 第0梯隊：短時間內免費 ===
             //"tencent/hy3:free",                          //無法傳出
-
+            "inclusionai/ling-3.0-flash:free", //測試新的MoE模型
 
 
             // === 第一梯隊：大型高品質模型 ===
